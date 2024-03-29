40031d606afb6a6a2d35ee9fdfaa0ef64ebdb15c
Pazzword <anzor.pozner@gmail.com>
2024-03-29 01:09:50 +0000

    hamo page for mobile correction

--- pashaev/views.py
+++ pashaev/views.py
@@ -11,17 +11,17 @@ showcasing his ability to convey deep emotions through a meticulous artistic pro
 whose profound expressions resonate with the complexities of human existence."""
 
 mod = """The artist's series of works, titled "Esoteric Perspective on Ancestral Symbols," is a profound exploration inspired by their late mentor, Vsevolod Semenych Grygoriev. Rooted in the artist's journey of self-discovery during the early 90s, these pieces reflect their deep immersion in avant-garde movements, particularly surrealism, as they sought to articulate their unique vision through art.
-                                                                                                                                            |
+                                                                                                                                                            |
 Drawing from their academic background in graphic design at the Chechen State Pedagogical Institute, the artist received pivotal guidance from Grygoriev, a seasoned artist with a rich theatrical background. Under Grygoriev's mentorship, the artist gained insights into the transformative impact of artists like Salvador Dali, who infused surrealism with their distinct worldview, shaping the artistic landscape of both the West and Europe.
-                                                                                                                                            |
+                                                                                                                                                            |
 Encouraged by Grygoriev to incorporate elements of national ethnicity into their work, the artist embarked on a creative journey marked by profound symbolism and cultural significance. Each piece in the series reflects a meticulous fusion of artistic expression and ancestral heritage, aimed at capturing the essence of the human experience."""
 
 mod2 = """At the heart of these artworks lies a symbolic tapestry of imagery. The winged sun disk, a recurring motif, symbolizes the radiance of the sun and the warmth it brings, evoking feelings of tranquility and peace. Adjacent to it stands a towering structure—an architectural emblem representing human ingenuity and craftsmanship.
-                                                                                                                                            |
+                                                                                                                                                        |
 On one side of the composition, the Kumgan—a vessel historically carried by women to transport water—takes center stage. Beyond its practical function, the Kumgan embodies profound symbolism, serving as a testament to compassion, femininity, and the nurturing essence of motherhood.
-                                                                                                                                            |
+                                                                                                                                                        |
 Conversely, the balalaika, known as "dechig pondar," occupies the opposite end of the composition—a poignant tribute to ethnic culture, philosophy, and wisdom. Through its melodic resonance, the balalaika encapsulates the soulful essence of tradition and heritage, inviting viewers to delve into its rich tapestry of meanings.
-                                                                                                                                            |
+                                                                                                                                                        |
 Unified by a central pictogram element reminiscent of the sun's radiant energy, each artwork in the series serves as a testament to the artist's creative prowess and their profound connection to ancestral symbols. Through this captivating exploration, the artist invites viewers on a journey of introspection and discovery, where art becomes a conduit for understanding the complexities of the human condition."""
 
 sign = """In this artwork, one encounters a depiction from the series "An Esoteric Perspective on 
