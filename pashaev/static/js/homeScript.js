9aade1dd790f1b2cb79b47e8a7fdaf42120c6360
Pazzword <anzor.pozner@gmail.com>
2024-04-18 19:36:37 +0100

    change

--- pashaev/static/js/homeScript.js
+++ pashaev/static/js/homeScript.js
@@ -20,74 +20,29 @@ updateCarousel(); // Start the slideshow
 
 
 
-  // Top Image Slider
-function preloadImages() {
-    const images = [
-        "static/Images/homeSlides/IMG26.jpg",
-        "static/Images/homeSlides/IMG27.jpg",
-        "static/Images/homeSlides/IMG28.jpg",
-        "static/Images/homeSlides/IMG29.jpg",
-        "static/Images/homeSlides/IMG30.jpg"
-    ];
 
-    images.forEach(src => {
-        const img = new Image();
-        img.src = src;
-    });
-}
-
-// Call preload function when the page loads
-
-
-const track = document.getElementById("image-track");
-
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
-
-const handleOnUp = () => {
-track.dataset.mouseDownAt = "0";  
-track.dataset.prevPercentage = track.dataset.percentage;
-};
-
-const handleOnMove = e => {
-if (track.dataset.mouseDownAt === "0") return;
-
-const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-      maxDelta = window.innerWidth / 2;
-
-const percentage = (mouseDelta / maxDelta) * -100,
-      nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-      nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-
-track.dataset.percentage = nextPercentage;
-
-track.animate({
-  transform: `translate(${nextPercentage}%, -50%)`
-}, { duration: 1200, fill: "forwards" });
-
-for (const image of track.getElementsByClassName("images")) {
-  image.animate({
-    objectPosition: `${100 + nextPercentage}% center`
-  }, { duration: 1200, fill: "forwards" });
-}
-};
-
-
-/* --  extra lines for touch events -- */
-
-window.onmousedown = e => handleOnDown(e);
-
-window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
-
-
-
+//SLIDER//
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
 
 
 
