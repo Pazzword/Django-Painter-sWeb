43b68ffb97b7bf38ac65a7816e8005d9fb5f0f36
Anzor <anzor@Anzors-iMac.local>
2024-06-15 14:35:38 +0100

    wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add your project directory to the sys.path
+project_home = u'/home/pashaev/pashaev.pythonanywhere.com'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# Set the settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+# Activate your virtual env
+activate_this = '/home/pashaev/.virtualenvs/pashaev.pythonanywhere.com/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
 application = get_wsgi_application()
